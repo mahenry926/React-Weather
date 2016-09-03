23f0f30cced0ec392745ff587fa67d97d9dcacf2
mahenry <user@Matthew-Henrys-rMBP.local>
2016-09-03 12:29:10 -0700

    Styled About page, added resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,18 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page.</p>
+      <h1 className="text-center">About</h1>
+      <p>This is a weather application built with React.  I am just trying to learn.</p>
+      <p>
+        <ul>
+          <li>
+            <a href="https://facebook.github.io/react">React</a> - This was the the JS framework used.
+          </li>
+          <li>
+            <a href="https://openweathermap.org">Open Weather Map</a> - The source for weather information.
+          </li>
+        </ul>
+      </p>
     </div>
   );
 };
